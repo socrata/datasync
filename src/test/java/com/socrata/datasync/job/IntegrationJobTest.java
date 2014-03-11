5518bc53abacad611ea04dbe29606bebe19d3893
Adrian Laurenzi <adrian.laurenzi@gmail.com>
2014-03-11 14:46:52 -0700

    added unit tests for job file serialization changes and SmartUpdate integration tests

--- src/test/java/com/socrata/datasync/job/IntegrationJobTest.java
+++ src/test/java/com/socrata/datasync/job/IntegrationJobTest.java
@@ -22,9 +22,10 @@ public class IntegrationJobTest extends TestBase {
 
     public static final String PATH_TO_SAVED_JOB_FILE_V0dot1 = "src/test/resources/job_saved_v0.1.sij";
     public static final String PATH_TO_SAVED_JOB_FILE_V0dot3 = "src/test/resources/job_saved_v0.3.sij";
+    public static final String PATH_TO_SAVED_JOB_FILE_V0dot4 = "src/test/resources/job_saved_v0.4.sij";
 
     @Test
-    public void testOldJobFileDeserialization() throws IOException {
+    public void testDataSyncV0dot1JobFileDeserialization() throws IOException {
         IntegrationJob job = new IntegrationJob(PATH_TO_SAVED_JOB_FILE_V0dot1);
 
         TestCase.assertEquals(
@@ -35,13 +36,13 @@ public class IntegrationJobTest extends TestBase {
         TestCase.assertEquals(
                 "src/test/resources/job_saved_v0.1.sij",
                 job.getPathToSavedFile());
+        TestCase.assertEquals(false, job.getPublishViaFTP());
     }
 
     @Test
-    public void testNewJobFileDeserialization() throws IOException {
+    public void testDataSyncJSONJobFileDeserialization() throws IOException {
         IntegrationJob job = new IntegrationJob(PATH_TO_SAVED_JOB_FILE_V0dot3);
 
-        TestCase.assertEquals(1, job.getFileVersionUID());
         TestCase.assertEquals(
                 "/Users/adrian/Dropbox/Socrata_s/projects/data_integration/datasync/misc/honolulu_report2.csv",
                 job.getFileToPublish());
@@ -50,30 +51,54 @@ public class IntegrationJobTest extends TestBase {
         TestCase.assertEquals(
                 "src/test/resources/job_saved_v0.3.sij",
                 job.getPathToSavedFile());
+        TestCase.assertEquals(false, job.getPublishViaFTP());
     }
 
     @Test
-    public void testIntegrationJobReplace() throws IOException, LongRunningQueryException, SodaError {
-        File configFile = new File(PATH_TO_CONFIG_FILE);
-        ObjectMapper mapper = new ObjectMapper();
-        UserPreferences userPrefs = mapper.readValue(configFile, UserPreferencesFile.class);
-        com.socrata.datasync.job.IntegrationJob jobToRun = new com.socrata.datasync.job.IntegrationJob(userPrefs);
+    public void testDataSyncJSONv0dot4JobFileDeserialization() throws IOException {
+        IntegrationJob job = new IntegrationJob(PATH_TO_SAVED_JOB_FILE_V0dot4);
+        TestCase.assertEquals("E:\\tm\\02.26.2014.17.59.22.0.0695675217148733.csv", job.getFileToPublish());
+        TestCase.assertEquals("kwgk-zc5k", job.getDatasetID());
+        TestCase.assertEquals(PublishMethod.replace, job.getPublishMethod());
+        TestCase.assertEquals("src/test/resources/job_saved_v0.4.sij", job.getPathToSavedFile());
+        TestCase.assertEquals(true, job.getFileToPublishHasHeaderRow());
+        TestCase.assertEquals(true, job.getPublishViaFTP());
+        TestCase.assertEquals("E:\\tm\\control.json", job.getPathToFTPControlFile());
+        TestCase.assertEquals("job_saved_v0.4.sij", job.getJobFilename());
+    }
+
+    @Test
+    public void testIntegrationJobReplaceViaHTTP() throws IOException, LongRunningQueryException, SodaError {
+        IntegrationJob jobToRun = getIntegrationJobWithUserPrefs();
         jobToRun.setDatasetID(UNITTEST_DATASET_ID);
         jobToRun.setFileToPublish("src/test/resources/datasync_unit_test_two_rows.csv");
         jobToRun.setPublishMethod(PublishMethod.replace);
         jobToRun.setFileToPublishHasHeaderRow(true);
         JobStatus status = jobToRun.run();
+        TestCase.assertEquals(false, jobToRun.getPublishViaFTP());
         TestCase.assertEquals(JobStatus.SUCCESS, status);
         TestCase.assertFalse(status.isError());
         TestCase.assertEquals(2, getTotalRows(UNITTEST_DATASET_ID));
     }
 
     @Test
+    public void testIntegrationJobReplaceViaFTP() throws IOException, LongRunningQueryException, SodaError {
+        IntegrationJob jobToRun = getIntegrationJobWithUserPrefs();
+        jobToRun.setDatasetID(UNITTEST_DATASET_ID);
+        jobToRun.setFileToPublish("src/test/resources/datasync_unit_test_three_rows.csv");
+        jobToRun.setPathToFTPControlFile("src/test/resources/datasync_unit_test_three_rows_control.json");
+        jobToRun.setPublishMethod(PublishMethod.replace);
+        jobToRun.setPublishViaFTP(true);
+        jobToRun.setFileToPublishHasHeaderRow(false);
+        JobStatus status = jobToRun.run();
+        TestCase.assertEquals(JobStatus.SUCCESS, status);
+        TestCase.assertFalse(status.isError());
+        TestCase.assertEquals(3, getTotalRows(UNITTEST_DATASET_ID));
+    }
+
+    @Test
     public void testIntegrationJobInvalidDatasetId() throws IOException, LongRunningQueryException, SodaError {
-        File configFile = new File(PATH_TO_CONFIG_FILE);
-        ObjectMapper mapper = new ObjectMapper();
-        UserPreferences userPrefs = mapper.readValue(configFile, UserPreferencesFile.class);
-        com.socrata.datasync.job.IntegrationJob jobToRun = new com.socrata.datasync.job.IntegrationJob(userPrefs);
+        IntegrationJob jobToRun = getIntegrationJobWithUserPrefs();
         jobToRun.setDatasetID("invalidid");
         jobToRun.setFileToPublish("src/test/resources/datasync_unit_test_two_rows.csv");
         jobToRun.setPublishMethod(PublishMethod.replace);
@@ -84,19 +109,31 @@ public class IntegrationJobTest extends TestBase {
     }
 
     @Test
-    public void testIntegrationJobPublishError() throws IOException, LongRunningQueryException, SodaError {
-        File configFile = new File(PATH_TO_CONFIG_FILE);
-        ObjectMapper mapper = new ObjectMapper();
-        UserPreferences userPrefs = mapper.readValue(configFile, UserPreferencesFile.class);
-        com.socrata.datasync.job.IntegrationJob jobToRun = new com.socrata.datasync.job.IntegrationJob(userPrefs);
+     public void testIntegrationJobPublishError() throws IOException, LongRunningQueryException, SodaError {
+        IntegrationJob jobToRun = getIntegrationJobWithUserPrefs();
         jobToRun.setDatasetID(UNITTEST_DATASET_ID);
         jobToRun.setFileToPublish("src/test/resources/datasync_unit_test_invalid_date.csv");
         jobToRun.setPublishMethod(PublishMethod.upsert);
         jobToRun.setFileToPublishHasHeaderRow(true);
+        jobToRun.setPublishViaFTP(false);
         JobStatus status = jobToRun.run();
         TestCase.assertEquals(JobStatus.PUBLISH_ERROR, status);
         TestCase.assertTrue(status.isError());
-        // TODO determine what error message should be
-        TestCase.assertTrue(status.getMessage().equals("Invalid..."));
+        TestCase.assertEquals("Unknown date format 'invalid'. (line 3 of file) \n", status.getMessage());
+    }
+
+    @Test
+    public void testIntegrationJobFTPPublishError() throws IOException, LongRunningQueryException, SodaError {
+        IntegrationJob jobToRun = getIntegrationJobWithUserPrefs();
+        jobToRun.setDatasetID(UNITTEST_DATASET_ID);
+        jobToRun.setFileToPublish("src/test/resources/datasync_unit_test_invalid_date.csv");
+        jobToRun.setPathToFTPControlFile("src/test/resources/datasync_unit_test_three_rows_control.json");
+        jobToRun.setFileToPublishHasHeaderRow(true);
+        jobToRun.setPublishMethod(PublishMethod.replace);
+        jobToRun.setPublishViaFTP(true);
+        JobStatus status = jobToRun.run();
+        TestCase.assertEquals(JobStatus.PUBLISH_ERROR, status);
+        TestCase.assertTrue(status.isError());
+        TestCase.assertEquals("FAILURE: Processing datasync_unit_test_invalid_date.csv failed: Uninterpretable value in CSV at record 2: Cannot parse invalid as any of [ \"MM/dd/yyyy\" ]\n", status.getMessage());
     }
 }
