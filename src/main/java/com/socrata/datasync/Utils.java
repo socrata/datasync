44d0f7cbecda29ebf599970ebe5f627a99592a6a
Robert J. Macomber <robert.macomber@socrata.com>
2017-04-26 15:53:05 -0700

    Put braces around if/else :(

--- src/main/java/com/socrata/datasync/Utils.java
+++ src/main/java/com/socrata/datasync/Utils.java
@@ -239,8 +239,11 @@ public class Utils {
         URI versionApiUri = new URI("https://" + DatasetUtils.getDomainWithoutScheme(domain) + VERSION_API_ENDPOINT);
         try(CloseableHttpResponse response = http.get(versionApiUri, ContentType.APPLICATION_JSON.getMimeType())) {
             Header[] headers = response.getHeaders(X_SOCRATA_REGION);
-            if(headers.length == 0) return "development";
-            else return headers[0].getValue();
+            if(headers.length == 0) {
+                return "development";
+            } else {
+                return headers[0].getValue();
+            }
         }
     }
 }
