44cb95842abae29fbf493dd34cc6173ef9e17209
Robert J. Macomber <robert.macomber@socrata.com>
2017-04-26 15:55:26 -0700

    More braces

--- src/main/java/com/socrata/datasync/config/userpreferences/UserPreferencesUtil.java
+++ src/main/java/com/socrata/datasync/config/userpreferences/UserPreferencesUtil.java
@@ -2,7 +2,10 @@ package com.socrata.datasync.config.userpreferences;
 
 public class UserPreferencesUtil {
     static String prefixDomain(String s) {
-        if(s.contains("//")) return s;
-        else return "https://" + s;
+        if(s.contains("//")) {
+            return s;
+        } else {
+            return "https://" + s;
+        }
     }
 }
